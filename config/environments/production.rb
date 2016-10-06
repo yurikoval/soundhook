d5b454bd59c38652551b0c06e669e6255a5b3e31
Yuri Kovalov <yuri@yurikoval.com>
2016-10-06 18:30:48 +0800

    Update domain urls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,8 +36,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://tg-soundhook.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://tg-soundhook.herokuapp.com', 'http://tg-soundhook.herokuapp.com']
+  config.action_cable.url = "wss://billboard.tradegecko.com/cable"
+  config.action_cable.allowed_request_origins = ['https://billboard.tradegecko.com', 'http://billboard.tradegecko.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
