6bbf23dd76c601b438aa55f43935f171cdb52bdb
Yuri Kovalov <yuri@yurikoval.com>
2016-10-06 15:25:42 +0800

    Add presence validation

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,5 +1,6 @@
 class Sound < ApplicationRecord
   validates_uniqueness_of :name
+  validates_presence_of :name, :sound_file
   has_attached_file :sound_file
   validates_attachment_content_type :sound_file, :content_type => /.*/
 end
