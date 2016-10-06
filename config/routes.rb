c408313bddab6158a7358faace34396a8bfaebf0
Yuri Kovalov <yuri@yurikoval.com>
2016-10-06 11:14:15 +0800

    Allow copying of webhook from index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :sounds
   match '/hook/:provider', to: 'hook#create', as: :hook, via: %i(get post)
   get 'tester/index'
-  root to: 'tester#index'
+  root to: 'sounds#index'
 end
