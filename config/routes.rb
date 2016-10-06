a11d842803b1f6419088a00b9c8fffdee96e0cb6
Yuri Kovalov <yuri@yurikoval.com>
2016-10-06 23:14:36 +0800

    Move board

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
   resources :sounds
   match '/hook/:provider', to: 'hook#create', as: :hook, via: %i(get post)
-  get 'tester/index'
+  get 'board' => 'tester#index'
+  get 'tester/index', to: redirect('/board')
   root to: 'sounds#index'
 end
