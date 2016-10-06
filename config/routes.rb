ed85e477191b0682ae63e2a40dac9ae4a5598a58
Yuri Kovalov <yuri@yurikoval.com>
2016-10-06 10:33:13 +0800

    Add soundhook test page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :sounds
   match '/hook/:provider', to: 'hook#create', as: :hook, via: %i(get post)
+  get 'tester/index'
+  root to: 'tester#index'
 end
