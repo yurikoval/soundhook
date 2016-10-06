3cec7246227295d3496a8a22a35c0a87c884ad51
Yuri Kovalov <yuri@yurikoval.com>
2016-10-06 23:25:34 +0800

    Dont require sound for youtube

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,10 +1,16 @@
 class Sound < ApplicationRecord
   validates_uniqueness_of :name
-  validates_presence_of :name, :sound_file
+  validates_presence_of :name
+  validates_presence_of :sound_file, if: :type_requires_sound
   has_attached_file :sound_file
   validates_attachment_content_type :sound_file, :content_type => /.*/
 
   before_save do
     self.hook_type ||= 'sound'
+  end
+
+private
+  def type_requires_sound
+    self.hook_type != 'youtube'
   end
 end
