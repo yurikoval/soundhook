f97cb6b6c1411dff336b00ae27723a922134150b
Yuri Kovalov <yuri@yurikoval.com>
2016-10-13 18:03:50 +0800

    Secure sounds with http auth

--- app/controllers/tester_controller.rb
+++ app/controllers/tester_controller.rb
@@ -1,4 +1,8 @@
 class TesterController < ApplicationController
+  if ENV['AUTH_USER'] && ENV['AUTH_PASS']
+    http_basic_authenticate_with name: ENV['AUTH_USER'], password: ENV['AUTH_PASS']
+  end
+
   def index
   end
 end
